cb0a621022bad93aa611e73d4d20a6c6f73854a4
nick9999 <nikhilpatil123@gmail.com>
2015-01-17 18:13:06 +0530

    String matching bug

--- debug.py
+++ debug.py
@@ -2,4 +2,10 @@ import pprint
 
 class debug:
 	def __init__(self):
-		
+		self.showerrormsg=False
+		self.printerror=True
+		self.linenumber=1
+
+	def printerror(self,statement):
+		if self.printerror:
+			print 'Error '
