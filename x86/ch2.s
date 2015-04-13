d052515957975491734e06a3c44fe484dd2c94a0
kumardeepakr3 <kumardeepakr3@gmail.com>
2015-04-13 21:33:51 +0530

    PrintNumber.s working properly

--- x86/ch2.s
+++ x86/ch2.s
@@ -1,35 +1,38 @@
+# imultest.s - An example of the IMUL instruction formats
 .section .data
-
 number:
-.long 7909090
-
+	.long 8888
+value1:
+.long 1
+value2:
+.long -1
+value3:
+.int 400
 .section .text
 .globl _start
 _start:
-
-
-
-#USAGE: Move the number to print in the %ecx register and call printIntNumber
-movl number, %ecx
+movl value1, %ebx
+movl value2, %ecx
+imul %ebx, %ecx
+#movl value3, %edx
+#imull $2, %edx, %eax
+movl $0, number
+movl %ecx, number
+movl %ecx, %ebx
 call printIntNumber
 
-#EXIT CODE
-movl $1, %eax         # system call number for exit
-movl $0, %ebx         # value written to terminal
-int $0x80 
+movl $0, %ebx
+#movl %ecx, %ebx
+movl $1, %eax
+int $0x80
 
-
-
-
-
-# USAGE: Put the neeche ka code in any portion of the text section
 
 jmp EndPrintNum
 printIntNumber:
-   #movl %ecx, %edi  #store the number in % edi as %ebx would be modified here
+   movl %ebx, %edi  #store the number in % edi as %ebx would be modified here
   
 # code to print the number -> same as in question 1    
-    movl %ecx, %eax #storing number in %eax to divide it by 10 and extract digits one by one                  
+    movl number, %eax #storing number in %eax to divide it by 10 and extract digits one by one                  
     movl %esp, %esi   #storing the initial position of the stack pointer in %esi register
     labl:
     #%eax has quotient after div and %edx has the remainder after division. div R1 does %eax / R1 
@@ -48,15 +51,15 @@ printIntNumber:
                # digit of our number as it was pushed last into the stack
     addl $48, %edx  # converting the digit to ascii character by adding 48 to that digit 
                    # as, 48 is the ascii code for digit 0, 49 for 1, 50 for 2 and so on...
-    pushl %edx # s is randomly chosen memory location to store it
+    movl %edx, number+50 # storing the corresponding ascii character into some memory adress
+                      #say (number +50) which is randomly chosen memory location to store it
                       # we store it in a memory location to print it by using sytem call no.4 
     
     movl $4, %eax  # 4 is sys call number
     movl $1, %ebx  # 1 refers to stdout
-    movl %esp, %ecx  # number+50 refers to location where the digit is stored in memory 
+    movl $(number+50), %ecx  # number+50 refers to location where the digit is stored in memory 
     movl $1, %edx    # size of buffer =1, as we print one digit at a time 
-    int $0x80
-    popl %edx       # Pop the digit on the top  
+    int $0x80    
     cmp %esp, %esi   # checking if the current stack pointer has reached the initial postion
                      # of stack pointer which we stored in %esi at the beginning, before 
                      # starting to push any value
@@ -64,6 +67,3 @@ printIntNumber:
                     #back to print_num label to pop more values and print them.
     ret  
 EndPrintNum:
-
-
-         
